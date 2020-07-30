8bf7af8f39d78072ef123220b3dbcafdeb3def73
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-07-30 14:47:54 -0300

    esconder menu ao descer a tela

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -2,3 +2,11 @@ function abrirMenu(){
     let menu = document.querySelector(".navbar");
     menu.classList.toggle('menu-toogle');
 }
+
+let posAnterior = window.pageYOffset;
+let menu = document.querySelector('.navbar');
+window.onscroll = function () {
+    let posAtual = window.pageYOffset;
+    posAnterior > posAtual ? menu.style.top = "0" : menu.style.top = "-70px";
+    posAnterior = posAtual;
+}
