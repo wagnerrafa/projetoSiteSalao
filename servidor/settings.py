06fbd347366c47842696d1145aa67166aa865e83
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-09-09 16:08:46 -0300

    configurar heroku

--- servidor/settings.py
+++ servidor/settings.py
@@ -130,4 +130,10 @@ EMAIL_HOST_USER = 'meuEmail'
 EMAIL_HOST_PASSWORD = 'minhaSenha'
 EMAIL_PORT = 587
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
