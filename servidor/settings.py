e44e3a45cef6ad9a41390033394e390157fdc17d
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-09-09 15:31:52 -0300

    configuracao do heroku

--- servidor/settings.py
+++ servidor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +128,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'meuEmail'
 EMAIL_HOST_PASSWORD = 'minhaSenha'
 EMAIL_PORT = 587
+
+django_heroku.settings(locals())
